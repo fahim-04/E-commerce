652bf7179e08566fba159fdc2dcb2a71e0ee2b68
Fahim Faysal <fahim.faysal0012@gmail.com>
2024-11-05 17:02:21 +0600

    updated nav bar

--- admin/web/navbar.php
+++ admin/web/navbar.php
@@ -54,6 +54,20 @@
 			</div>
 		</div>
 	</div>
+	<div class="mt-search-popup">
+		<div class="mt-holder">
+			<a href="#" class="search-close"><span></span><span></span></a>
+			<div class="mt-frame">
+				<form action="#">
+					<fieldset>
+						<input type="text" placeholder="Search...">
+						<button class="icon-magnifier" type="submit"></button>
+
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</div>
 	<!-- mt bottom bar end here -->
 	<span class="mt-side-over"></span>
 </header>
